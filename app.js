58226a2f552a6b32b3937687f083acf1b748bc8d
HASAN SAKALLI <89208116+hasansakalli@users.noreply.github.com>
2022-05-06 01:22:00 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,11 +6,18 @@ const message = document.querySelector(".message");
 function openBox() {
   container.classList.toggle("containerActive");
   icon.style.display = "none";
+
+  const myTimeout = setTimeout(openText, 3000);
+  function openText() {
+    document.getElementById("otoMessage").innerHTML =
+      "Guten Morgen <br> Wie kann ich Ihnen helfen?";
+  }
 }
 
 function closeBox() {
   container.classList.toggle("containerActive");
   icon.style.display = "block";
+  window.location.reload();
 }
 
 function send() {
